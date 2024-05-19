04dddb279c5fd5c276ca5c08cdbfe896210ba5a6
legoMaster5 <legom2023@gmail.com>
2024-05-19 21:06:30 +1100

    clear code

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-const main_link = document.querySelector('.mainLink');
-const works_link = document.querySelector('.worksLink');
-const inspiration_link = document.querySelector('.inspirationLink');
-const links_link = document.querySelector('.linksLink');
-
-const info = document.querySelector('#mainInfo');
+const info = document.querySelector('#main-info');
 const works = document.querySelector('#works');
 const inspiration = document.querySelector('#inspiration');
-const linksPage = document.querySelector('#linkPage');
+const linksPage = document.querySelector('#link-page');
 
 const site = document.querySelector('.site');
 
 const music = ['Driving', 'The Stream Hardphonk', 'Liquid', 'Pervasion', 'Space', 'Tear down', 'Worldtrip'];
-const buttons = document.querySelectorAll('.actionBtn');
+const buttons = document.querySelectorAll('.action-btn');
 const audio = document.querySelector('audio');
 
 const links = document.querySelectorAll('.link');
@@ -22,11 +17,11 @@ let play = "", pause = "";
 const media = window.matchMedia('(max-width: 1080px)')
 
 if (media.matches) {
-    play = "url(images/playMobile.png)";
-    pause = "url(images/pauseMobile.png)";
+    play = 'url(images/playMobile.png)';
+    pause = 'url(images/pauseMobile.png)';
 } else {
-    play = "url(images/play.png)";
-    pause = "url(images/pause.png)";
+    play = 'url(images/play.png)';
+    pause = 'url(images/pause.png)';
 }
 
 
@@ -59,5 +54,7 @@ buttons.forEach((btn) => {
 });
 
 $(document).ready(() => {
-    $('.images').bxSlider({});
+    $('.images').bxSlider({
+        captions: true
+    });
 });
