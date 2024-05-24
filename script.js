65ccd326f0d0d76699fc6a3555e319ed3b8d59ca
legoMaster5 <168335515+legoMaster5@users.noreply.github.com>
2024-05-24 18:57:52 +1100

    test

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ let play = "", pause = "";
 
 const media = window.matchMedia('(max-width: 1080px)');
 
-screen.orientation.lock("portrait");
+document.documentElement.requestFullscreen().then(() => screen.orientation.lock("portrait"));
 
 if (media.matches) {
     play = 'url(images/playMobile.png)';
